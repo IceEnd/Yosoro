0b086412f24cbf2514d39225d33e825f1c91c951
IceEnd <min@coolecho.net>
2018-06-18 21:49:12 +0800

    linux deb

---
+++ scripts/packageDeb.js
@@ -0,0 +1,23 @@
+import path from 'path';
+import installer from 'electron-installer-debian';
+import { productName, version, name } from '../package.json'
+
+const options = {
+  bin: 'Yosoro',
+  // name: 'Yosoro',
+  productName,
+  genericName: productName,
+  categories: ['Utility'],
+  src: path.resolve(__dirname, '../out', `${productName}-linux-x64`),
+  dest: path.resolve(__dirname, '../out', `${productName}-linux-x64-deb-${version}`),
+  arch: 'amd64',
+  icon: path.resolve(__dirname, '../app/main/resource/app.png'),
+  homepage: 'https://yosoro.coolecho.net',
+};
+
+installer(options)
+  .then(() => console.info(`Successfully created package at ${options.dest}`))
+  .catch((err) => {
+    console.error(err, err.stack);
+    process.exit(1);
+  });
