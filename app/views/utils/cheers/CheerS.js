17d37d8b0e6bb57b177899618a9a6e41bec5c155
IceEnd <min@coolecho.net>
2019-01-17 20:41:13 +0800

    table token

--- app/views/utils/cheers/CheerS.js
+++ app/views/utils/cheers/CheerS.js
@@ -4,6 +4,10 @@ import 'codemirror/addon/scroll/simplescrollbars.css';
 import 'codemirror/mode/htmlmixed/htmlmixed';
 import 'codemirror/mode/lua/lua';
 import 'codemirror/mode/stex/stex';
+import 'codemirror/mode/php/php';
+import 'codemirror/mode/python/python';
+import 'codemirror/mode/sql/sql';
+import 'codemirror/mode/go/go';
 
 import 'codemirror/addon/fold/markdown-fold';
 import 'codemirror/addon/scroll/simplescrollbars';
--- app/views/utils/cheers/mode/markdown.js
+++ app/views/utils/cheers/mode/markdown.js
@@ -98,9 +98,10 @@ CodeMirror.defineMode('markdown', (cmCfg, modeCfg) => {
   // const atxHeaderRE = /^(#+)(?: |$) +/;
   const atxLatexRE = /^(\$+)\s*$/;
   const setextHeaderRE = /^ *(?:={1,}|-{1,})\s*$/;
-  const textRE = /^[^#![\]*_\\<>` "'(~:$]+/;
+  const textRE = /^[^#![\]*_\\<>` "'(~:$|]+/;
   const fencedCodeRE = /^(~~~+|```+)[ \t]*([\w+#-]*)[^\n`]*$/;
   const linkDefRE = /^\s*\[[^\]]+?\]:.*$/; // naive link-definition
+  const tableRE = /^\s*\|[^|]+\|.+\|\s*$/; // Table
   const punctuation = /[!"#$%&'()*+,\-./:;<=>?@[\\\]^_`{|};~\xA1\xA7\xAB\xB6\xB7\xBB\xBF\u037E\u0387\u055A-\u055F\u0589\u058A\u05BE\u05C0\u05C3\u05C6\u05F3\u05F4\u0609\u060A\u060C\u060D\u061B\u061E\u061F\u066A-\u066D\u06D4\u0700-\u070D\u07F7-\u07F9\u0830-\u083E\u085E\u0964\u0965\u0970\u0AF0\u0DF4\u0E4F\u0E5A\u0E5B\u0F04-\u0F12\u0F14\u0F3A-\u0F3D\u0F85\u0FD0-\u0FD4\u0FD9\u0FDA\u104A-\u104F\u10FB\u1360-\u1368\u1400\u166D\u166E\u169B\u169C\u16EB-\u16ED\u1735\u1736\u17D4-\u17D6\u17D8-\u17DA\u1800-\u180A\u1944\u1945\u1A1E\u1A1F\u1AA0-\u1AA6\u1AA8-\u1AAD\u1B5A-\u1B60\u1BFC-\u1BFF\u1C3B-\u1C3F\u1C7E\u1C7F\u1CC0-\u1CC7\u1CD3\u2010-\u2027\u2030-\u2043\u2045-\u2051\u2053-\u205E\u207D\u207E\u208D\u208E\u2308-\u230B\u2329\u232A\u2768-\u2775\u27C5\u27C6\u27E6-\u27EF\u2983-\u2998\u29D8-\u29DB\u29FC\u29FD\u2CF9-\u2CFC\u2CFE\u2CFF\u2D70\u2E00-\u2E2E\u2E30-\u2E42\u3001-\u3003\u3008-\u3011\u3014-\u301F\u3030\u303D\u30A0\u30FB\uA4FE\uA4FF\uA60D-\uA60F\uA673\uA67E\uA6F2-\uA6F7\uA874-\uA877\uA8CE\uA8CF\uA8F8-\uA8FA\uA8FC\uA92E\uA92F\uA95F\uA9C1-\uA9CD\uA9DE\uA9DF\uAA5C-\uAA5F\uAADE\uAADF\uAAF0\uAAF1\uABEB\uFD3E\uFD3F\uFE10-\uFE19\uFE30-\uFE52\uFE54-\uFE61\uFE63\uFE68\uFE6A\uFE6B\uFF01-\uFF03\uFF05-\uFF0A\uFF0C-\uFF0F\uFF1A\uFF1B\uFF1F\uFF20\uFF3B-\uFF3D\uFF3F\uFF5B\uFF5D\uFF5F-\uFF65]|\uD800[\uDD00-\uDD02\uDF9F\uDFD0]|\uD801\uDD6F|\uD802[\uDC57\uDD1F\uDD3F\uDE50-\uDE58\uDE7F\uDEF0-\uDEF6\uDF39-\uDF3F\uDF99-\uDF9C]|\uD804[\uDC47-\uDC4D\uDCBB\uDCBC\uDCBE-\uDCC1\uDD40-\uDD43\uDD74\uDD75\uDDC5-\uDDC9\uDDCD\uDDDB\uDDDD-\uDDDF\uDE38-\uDE3D\uDEA9]|\uD805[\uDCC6\uDDC1-\uDDD7\uDE41-\uDE43\uDF3C-\uDF3E]|\uD809[\uDC70-\uDC74]|\uD81A[\uDE6E\uDE6F\uDEF5\uDF37-\uDF3B\uDF44]|\uD82F\uDC9F|\uD836[\uDE87-\uDE8B]/;
   const expandedTab = '    '; // CommonMark specifies tab as 4 spaces
 
@@ -108,6 +109,10 @@ CodeMirror.defineMode('markdown', (cmCfg, modeCfg) => {
     ')': /^(?:[^\\()]|\\.|\((?:[^\\()]|\\.)*\))*?(?=\))/,
     ']': /^(?:[^\\[\]]|\\.|\[(?:[^\\[\]]|\\.)*\])*?(?=\])/,
   };
+
+  function isLineEnd(stream) {
+    return stream.string.length === stream.pos;
+  }
 
   // switch mode
   function switchMode(state, mode) {
@@ -478,6 +483,83 @@ CodeMirror.defineMode('markdown', (cmCfg, modeCfg) => {
         type = '';
       }
       return type + tokenTypes.linkEmail;
+    }
+
+    if (ch === '|' && tableRE.test(stream.string)) {
+      stream.pos = stream.start + 1;
+      let istable = true;
+      let rowStyles;
+      let isEnd = false;
+
+      let nextLine = stream.lookAhead(1);
+      if (tableRE.test(nextLine)) {
+        nextLine = nextLine.replace(/^\s*\|/, '').replace(/\|\s*$/, '');
+      } else {
+        if (state.table) {
+          isEnd = true;
+        }
+        istable = false;
+      }
+
+      if (istable) {
+        rowStyles = nextLine.split('|');
+        for (let i = 0; i < rowStyles.length; i++) {
+          let row = rowStyles[i];
+          if (/^\s*--+\s*:\s*$/.test(row)) {
+            row = 'right';
+          } else if (/^\s*:\s*--+\s*$/.test(row)) {
+            row = 'left';
+          } else if (/^\s*:\s*--+\s*:\s*$/.test(row)) {
+            row = 'center';
+          } else if (/^\s*--+\s*$/.test(row)) {
+            row = 'default';
+          } else {
+            istable = false;
+            break;
+          }
+          rowStyles[i] = row;
+        }
+      }
+
+      if (istable) {
+        state.tableColumns = rowStyles;
+        state.tableID = `T${stream.lineOracle.line}`;
+        state.tableRow = 0;
+        state.tableCol = 0;
+        state.table = true;
+      }
+
+      if (state.table) {
+        state.tableCol = 0;
+      }
+
+      if (istable || state.table) {
+        const colUbound = state.tableColumns.length - 1;
+        let types = '';
+        if ((state.tableCol === 0 && /^\s*\|$/.test(stream.string.slice(0, stream.pos))) || stream.match(/^\s*$/, false)) {
+          types += 'table-sep table-sep-dummy';
+        } else if (state.tableCol < colUbound) {
+          const row = state.tableRow;
+          const col = state.tableCol++;
+          if (col === 0) {
+            types += ` line-table_${state.tableID} line-table-row line-table-row-${row}`;
+          }
+          types += ` table-sep table-sep-${col}`;
+        }
+        if (istable) {
+          state.table = true;
+        }
+        if (isLineEnd(stream)) {
+          state.tableRow++;
+        }
+        if (isEnd && isLineEnd(stream)) {
+          state.table = false;
+          state.tableCol = 0;
+          state.tableRow = 0;
+          state.tableColumns = [];
+        }
+        return types;
+      }
     }
 
     if (modeCfg.xml && ch === '<' && stream.match(/^(!--|\?|!\[CDATA\[|[a-z][a-z0-9-]*(?:\s+[a-z_:.-]+(?:\s*=\s*[^>]+)?)*\s*(?:>|$))/i, false)) {
@@ -1006,6 +1088,13 @@ CodeMirror.defineMode('markdown', (cmCfg, modeCfg) => {
         strikethrough: false,
         emoji: false,
         fencedEndRE: null,
+
+        // table
+        table: false,
+        tableCol: 0,
+        tableRow: 0,
+        tableColumns: [],
+        tableID: null,
       };
     },
 
@@ -1052,6 +1141,12 @@ CodeMirror.defineMode('markdown', (cmCfg, modeCfg) => {
         trailingSpaceNewLine: s.trailingSpaceNewLine,
         md_inside: s.md_inside,
         fencedEndRE: s.fencedEndRE,
+
+        table: s.table,
+        tableCol: s.tableCol,
+        tableRow: s.tableRow,
+        tableColumns: s.tableColumns,
+        tableID: s.tableId,
       };
     },
 
